3eca66b6bb911f424c5e2d1d3bdcd97cb3bf54fd
luminorena <bakhuta.oa@gmail.com>
2022-10-09 01:17:45 +0300

    #1 Parametrized Tests

--- src/test/java/tests/WildberriesParamsTests.java
+++ src/test/java/tests/WildberriesParamsTests.java
@@ -1,9 +1,8 @@
 package tests;
 
-import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.CollectionCondition;
 import data.ItemName;
 import data.MainElements;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.*;
 
@@ -18,8 +17,7 @@ public class WildberriesParamsTests {
 
     @ValueSource(strings = {"платье", "брюки"})
     @ParameterizedTest(name = "Проверка поиска по категориям для запроса {0}")
-    void searchItemsTests(String testData){
-        Configuration.holdBrowserOpen = true;
+    void searchItemsTests(String testData) {
         open("https://www.wildberries.ru/");
         $("#searchInput").setValue(testData).pressEnter();
         $(".goods-name").shouldHave(text(testData));
@@ -28,43 +26,39 @@ public class WildberriesParamsTests {
     @CsvSource({"платье, По запросу «платье» найдено", "брюки, По запросу «брюки» найдено"})
     @ParameterizedTest(name = "Проверка отображения результатов по запросу {0}")
     void checkGeneralResultsTests(String searchQuery, String resultString) {
-        Configuration.holdBrowserOpen = true;
         open("https://www.wildberries.ru/");
         $("#searchInput").setValue(searchQuery).pressEnter();
         $(".searching-results__title").shouldHave(text(resultString));
     }
 
     static Stream<Arguments> arraySearchTestData() {
-            return Stream.of(
-                    Arguments.of(ItemName.платье, List.of("Крестильные платья"
-                            , "Крестильные платья для малышей")),
-                    Arguments.of(ItemName.брюки, List.of("Брюки",
-                            "Брюки для малышей"))
-            );
+        return Stream.of(
+                Arguments.of(ItemName.платье, List.of("Крестильные платья"
+                        , "Крестильные платья для малышей")),
+                Arguments.of(ItemName.брюки, List.of("Брюки",
+                        "Брюки для малышей"))
+        );
     }
-    @MethodSource ("arraySearchTestData")
+
+    @MethodSource("arraySearchTestData")
     @ParameterizedTest(name = "Проверка совпадения массива объектов по запросу {0}")
-    void checkArraySearchTests(ItemName itemName, List<String> objects){
-        Configuration.holdBrowserOpen = true;
+    void checkArraySearchTests(ItemName itemName, List<String> objects) {
         open("https://www.wildberries.ru/");
         $("#searchInput").setValue(itemName.name()).pressEnter();
-        /*$$x("//fieldset/label")
-                .filter(visible)
-                .shouldHave(CollectionCondition.texts(objects));*/
-        List<String> texts = $$x("//fieldset/label")
-                .filter(visible).texts();
-        Assertions.assertEquals(objects, texts);
-
+        $$("#filters .filter").get(0)
+                .$$(".filter__item")
+                .first(2)
+                .shouldHave(CollectionCondition.texts(objects));
     }
 
     @EnumSource(MainElements.class)
     @ParameterizedTest
     void checkLocale(MainElements mainElements) {
-        Configuration.holdBrowserOpen = true;
         open("https://www.wildberries.ru/");
         $$("a.navbar-pc__link.j-wba-header-item")
                 .find(text(mainElements.name())).shouldBe(visible);
 
     }
+
 
 }
